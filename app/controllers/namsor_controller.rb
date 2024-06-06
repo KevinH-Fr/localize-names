4188d6e1e03a19c3c4d268efac865aec678c4cc8
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-06-06 14:38:48 +0200

    basic name in field and call to namsor api

--- app/controllers/namsor_controller.rb
+++ app/controllers/namsor_controller.rb
@@ -3,20 +3,15 @@ class NamsorController < ApplicationController
     api_key = Rails.application.credentials.namsor[:api_key]
     namsor_service = NamsorService.new(api_key)
     
-    personal_names = [
-      {
-        id: "e630dda5-13b3-42c5-8f1d-648aa8a21c42",
-        name: "Keith Haring"
-      }
-    ]
-
-    response = namsor_service.origin_full_batch(personal_names)
-
-    if response.success?
-      @data = response.parsed_response
-    else
-      @error = response.message
+    if params[:name].present?
+      personal_names = [{ name: params[:name] }]
+      response = namsor_service.origin_full_batch(personal_names)
+    
+      if response.success?
+        @data = response.parsed_response
+      else
+        @error = response.message
+      end
     end
   end
-
 end
